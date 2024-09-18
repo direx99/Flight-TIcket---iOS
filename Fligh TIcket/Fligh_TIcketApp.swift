451f720912e6abc8bf4a0efb50c5ea99c4b4de9b
direx99 <dinith.dash@gmail.com>
2024-09-18 22:02:15 +0530

    Initial Commit

---
+++ Fligh TIcket/Fligh_TIcketApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fligh_TIcketApp.swift
+//  Fligh TIcket
+//
+//  Created by Dinith Hasaranga on 2024-09-18.
+//
+
+import SwiftUI
+
+@main
+struct Fligh_TIcketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
